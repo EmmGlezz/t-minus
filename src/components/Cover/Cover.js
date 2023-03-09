3712d994d1e1f3685edd48ff658b3bbd12a9e6a2
EmmGlezz <contact@emilianogonzalez.com>
2023-03-09 20:26:00 +1100

    Cover Responsive

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -5,8 +5,8 @@ const Cover = () => {
   return (
     <div className='cover'>
         <div className='layer'>
-           <h1><span className='title text-center'>T-MINUS:</span> Countdown to the future of space exploration</h1>
-           <button>Upcoming Launches</button>
+           <h1 className='text-center'><span className='title'>T-MINUS:</span> Countdown to the future of space exploration</h1>
+           <button className='w-50'>Upcoming Launches</button>
         </div>
     </div>
   )
